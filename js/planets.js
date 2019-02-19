8afbd6d375356aaa867f15be82dd7473c469809b
Kyle <kkroberts1635@gmail.com>
2019-02-19 14:52:54 -0500

    Planet demo made

---
+++ js/planets.js
@@ -0,0 +1,88 @@
+function init(){
+    var scene = new THREE.Scene();
+    var gui = new dat.GUI();
+
+    var ambLight = new THREE.AmbientLight(0xffffff);
+    scene.add(ambLight);
+
+    var sun = createSphere(695, 0xffdd00);
+    scene.add(sun);
+
+    var mercury = createSphere(2.4, 0xffc68a);
+    mercury.name = "mercury";
+
+    var venus = createSphere(6, 0xffb58a);
+    venus.name = "venus";
+
+    var earth = createSphere(6.3, 0xaebaff);
+    earth.name = "earth";
+
+    var mars = createSphere(3.9, 0xff5246);
+    mars.name = "mars";
+
+    var jupiter = createSphere(69.9, 0xd96d42);
+    jupiter.name = "jupiter";
+
+    var saturn = createSphere(58.2, 0xd4c860);
+    saturn.name = "saturn";
+    
+    var uranus = createSphere(25.3, 0x92ccd6);
+    uranus.name = "uranus";
+
+    var neptune = createSphere(24.6, 0x2f54d9);
+    neptune.name = "neptune";
+
+    scene.add(mercury, venus, earth, mars, jupiter, saturn, uranus, neptune);
+
+    var camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        1, 
+        9000
+    );
+
+    camera.position.x = 2000;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    var renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.shadowMap.enabled = true;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000);
+    document.getElementById('webgl').appendChild(renderer.domElement);
+
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    update(renderer, scene, camera, controls);
+
+    return scene;
+}
+
+function planetOrbit(scene, planet ,timestamp, speed, orbit){
+    scene.getObjectByName(planet).position.x = Math.cos(timestamp * speed) * (695 + orbit);
+    scene.getObjectByName(planet).position.z = Math.sin(timestamp * speed) * (695 + orbit);
+}
+
+function update(renderer, scene, camera, controls){
+    renderer.render(
+        scene, camera
+    );
+
+    let timestamp = Date.now() * 0.0001;
+
+    planetOrbit(scene, "mercury", timestamp, 47.9, 36); //88
+    planetOrbit(scene, "venus", timestamp, 35, 67); //224.7
+    planetOrbit(scene, "earth", timestamp, 29.8, 141); //365.3
+    planetOrbit(scene, "mars", timestamp, 24, 438); //687
+    planetOrbit(scene, "jupiter", timestamp, 13.1, 886); //4343.5
+    planetOrbit(scene, "saturn", timestamp, 9.7, 1784); //10767.5 
+    planetOrbit(scene, "uranus", timestamp, 6.8, 2794); //30660
+    planetOrbit(scene, "neptune", timestamp, 5.4, 3674); //60152
+
+    requestAnimationFrame(function() {
+        update(renderer, scene, camera, controls);
+    });
+}
+
+var scene = init();
