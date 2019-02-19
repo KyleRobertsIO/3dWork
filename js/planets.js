505a34d2aee0fdb3ddc7aa179b17d01dbd9ed2e2
Kyle <kkroberts1635@gmail.com>
2019-02-19 15:14:09 -0500

    saturn has ring now

--- js/planets.js
+++ js/planets.js
@@ -3,6 +3,7 @@ function init(){
     var gui = new dat.GUI();
 
     var ambLight = new THREE.AmbientLight(0xffffff);
+    ambLight.position.y = 1000;
     scene.add(ambLight);
 
     var sun = createSphere(695, 0xffdd00);
@@ -23,7 +24,13 @@ function init(){
     var jupiter = createSphere(69.9, 0xd96d42);
     jupiter.name = "jupiter";
 
+    var geometry = new THREE.CylinderGeometry( 100, 100, 1, 40 );
+    var material = new THREE.MeshBasicMaterial( {color: 0xa14f10} );
+    var ring = new THREE.Mesh( geometry, material );
+    scene.add( ring );
+
     var saturn = createSphere(58.2, 0xd4c860);
+    saturn.add(ring);
     saturn.name = "saturn";
     
     var uranus = createSphere(25.3, 0x92ccd6);
@@ -38,7 +45,7 @@ function init(){
         45,
         window.innerWidth / window.innerHeight,
         1, 
-        9000
+        20000
     );
 
     camera.position.x = 2000;
@@ -60,6 +67,10 @@ function init(){
 }
 
 function planetOrbit(scene, planet ,timestamp, speed, orbit){
+    if(planet == "saturn"){
+        scene.getObjectByName(planet).rotation.x += 0.1;
+        scene.getObjectByName(planet).rotation.y += 0.1;
+    }
     scene.getObjectByName(planet).position.x = Math.cos(timestamp * speed) * (695 + orbit);
     scene.getObjectByName(planet).position.z = Math.sin(timestamp * speed) * (695 + orbit);
 }
@@ -71,14 +82,14 @@ function update(renderer, scene, camera, controls){
 
     let timestamp = Date.now() * 0.0001;
 
-    planetOrbit(scene, "mercury", timestamp, 47.9, 36); //88
-    planetOrbit(scene, "venus", timestamp, 35, 67); //224.7
-    planetOrbit(scene, "earth", timestamp, 29.8, 141); //365.3
-    planetOrbit(scene, "mars", timestamp, 24, 438); //687
-    planetOrbit(scene, "jupiter", timestamp, 13.1, 886); //4343.5
-    planetOrbit(scene, "saturn", timestamp, 9.7, 1784); //10767.5 
-    planetOrbit(scene, "uranus", timestamp, 6.8, 2794); //30660
-    planetOrbit(scene, "neptune", timestamp, 5.4, 3674); //60152
+    planetOrbit(scene, "mercury", timestamp, 47.9, 36); 
+    planetOrbit(scene, "venus", timestamp, 35, 67); 
+    planetOrbit(scene, "earth", timestamp, 29.8, 141); 
+    planetOrbit(scene, "mars", timestamp, 24, 438); 
+    planetOrbit(scene, "jupiter", timestamp, 13.1, 886); 
+    planetOrbit(scene, "saturn", timestamp, 9.7, 1784);
+    planetOrbit(scene, "uranus", timestamp, 6.8, 2794); 
+    planetOrbit(scene, "neptune", timestamp, 5.4, 3674); 
 
     requestAnimationFrame(function() {
         update(renderer, scene, camera, controls);
