33dfe06183ddd267caea10d456d4448ca8a9a653
Kyle <kkroberts1635@gmail.com>
2019-02-25 10:42:42 -0500

    fixed bug where a hill object is added multiple times to the array of hills

--- js/objectControl.js
+++ js/objectControl.js
@@ -1,43 +1,54 @@
-function randomTreeSpawns(scene) {
+function randomTreeSpawns(hillObj) {
     let treeArray = [];
     let trees = new THREE.Group();
     position = 0;
     for (let i = 0; i < 5; i++) {
-
-        var tree = treeGen();
-        tree.name = "tree-" + i;
-        tree.position.y = 1.25;
-        tree.position.x = position;
+        var tree = createTree();
+        tree.model.position.y = 1.25;
+        tree.model.position.x = position;
         position -= 5;
-
-        let rngPosition = Math.random() * (10 - (-10)) + (-10);
-        tree.position.z = rngPosition;
-
+        let rngPosition = Math.floor(Math.random() * (10 - (-10)) + (-10));
+        tree.model.position.z = rngPosition;
         treeArray.push(tree);
-        trees.add(tree);
+        trees.add(tree.model);
     }
-    scene.add(trees);
+
+    let treePositionsZ = [];
+    for (let i = 0; i < treeArray.length; i++) {
+        treePositionsZ.push(treeArray[i].model.position.z);
+    }
+    let posZ = Math.max.apply(null, treePositionsZ);
+    let negZ = Math.min.apply(null, treePositionsZ);
+
+
+
     trees.position.x = -7;
+    let obj = {
+        model: trees,
+        posZ: posZ,
+        negZ: negZ
+    }
+    return obj;
 }
 
-function buildChunk(scene) {
-    let CHUNK = new THREE.Group();
-
-
+function createHills() {
     let hillArray = [];
+    let hillClusters = new THREE.Group();
     let hillCount = Math.floor(Math.random() * (4 - 1) + 1);
+    console.log(hillCount);
 
     for (let i = 0; i < hillCount; i++) {
         let hillObject = rngHills();
         let randomHillZ = Math.floor(Math.random() * (80 - (-80)) + (-80));
         if (i == 0) {
             hillObject.model.position.z = randomHillZ;
-            CHUNK.add(hillObject.model);
+            hillClusters.add(hillObject.model);
             hillArray.push(hillObject);
-        } else if(i > 0) {
+        } else if (i > 0) {
             let currentPosZ = randomHillZ + hillObject.posZ;
             let currentNegZ = randomHillZ - hillObject.negZ;
-            for (let j = 0; j < hillCount; j++) {
+            let validPosition = false;
+            for (let j = 0; j < hillArray.length; j++) {
                 let prevPosZ = hillArray[j].model.position.z + hillArray[j].posZ;
                 let prevNegZ = hillArray[j].model.position.z - hillArray[j].negZ;
                 if (
@@ -45,19 +56,43 @@ function buildChunk(scene) {
                     ||
                     (prevPosZ > currentNegZ && prevNegZ < currentNegZ)
                 ) {
-                    console.log('fail');
                     i--; // Retry a generation
+                    validPosition = false;
                     break;
                 } else {
-                    hillObject.model.position.z = randomHillZ;
-                    CHUNK.add(hillObject.model);
-                    hillArray.push(hillObject);
+                    validPosition = true;
                 }
             }
+            if (validPosition == true) {
+                hillObject.model.position.z = randomHillZ;
+                hillClusters.add(hillObject.model);
+                hillArray.push(hillObject);
+            }
+
         }
     }
+    let obj = {
+        hills: hillArray,
+        model: hillClusters
+    }
+    return obj;
+}
 
+/*
+    Chunk Render Steps:
+    - Create hills
+    - Check if hills overlap
+    - Create tree and check is they overlap with hills
+*/
+function buildChunk(scene) {
+    let CHUNK = new THREE.Group();
+    let hillClusters = createHills();
+    CHUNK.add(hillClusters.model);
+
+    CHUNK.add(randomTreeSpawns(hillClusters).model);
+
+    CHUNK.position.x = -28;
     CHUNK.position.z = 0;
-    CHUNK.position.y = 50;
+    CHUNK.position.y = 0;
     scene.add(CHUNK);
 }
--- js/objectGeneration.js
+++ js/objectGeneration.js
@@ -1,16 +1,6 @@
-function hillGen() {
-    var hill = createSphere(14, 0x3c9e19);
-    hill.position.y = -7;
-    hill.position.x = -30;
-    hill.position.z = -80;
-    hill.name = "hill";
-    return hill;
-}
-
 function subHill(obj, bigHill) {
     let hillRadius = Math.floor((Math.random() * (12 - 3) + 3));
     let miniHill = createSphere(hillRadius, 0x3c9e19);
-
     let rng = Math.floor((Math.random() * 2) + 1);
     if (rng == 2) {
         let bigHillRadius = bigHill.geometry.parameters.radius;
@@ -30,11 +20,10 @@ function rngHills() {
     let generatedHills = Math.floor((Math.random() * 5) + 1);
     let objLength = 0;
     let firstHillRadius = 0;
-
     for (let i = 0; i < generatedHills; i++) {
         let radius = Math.floor(Math.random() * (15 - 3) + 3);
         let hill = createSphere(radius, 0x3c9e19);
-
+        hill.position.y = -radius / 2 * 0.8;
         if (hills.length > 0) {
             let prevHill = hills[i - 1].geometry.parameters.radius;
             if (radius > prevHill * 2) {
@@ -53,15 +42,10 @@ function rngHills() {
             hills.push(hill);
             hillsObj.add(hill);
         }
-
-
     }
-
     hillsObj.position.y = 0;
-
     let posZ = objLength;
     let negZ = -firstHillRadius;
-
     let hillCluster = {
         model: hillsObj,
         length: objLength,
@@ -71,94 +55,7 @@ function rngHills() {
     return hillCluster;
 }
 
-/*function randomHillGen() {
-    let hills = new THREE.Group();
-    let hillArray = [];
-    let hillCount = Math.floor((Math.random() * 15) + 1);
-    var addHill = true;
-    let hillsLength = 0;
-    let positions = [];
-
-    let firstLoop = true;
-    for (let i = 0; i < hillCount; i++) {
-        let radius = Math.floor(Math.random() * (15 - 3) + 3);
-        let hill = createSphere(radius, 0x3c9e19);
-        if (firstLoop == true) {
-            console.log(radius);
-            positions.push({
-                z: hill.position.z,
-                radius: radius
-            });
-            hillsLength += radius * 2;
-            firstLoop = false;
-        }
-
-        hill.position.x = -30;
-        hill.position.y = -radius / 2 * 0.7;
-
-        if (hillArray.length > 0) {
-            let prevHillRadius = hillArray[i - 1].geometry.parameters.radius;
-            if ((prevHillRadius * 2) > radius) {
-                let hillDirectionGen = Math.floor((Math.random() * 2) + 1);
-                switch (hillDirectionGen) {
-                    case 1:
-                        
-                        hill.position.z = hillArray[i - 1].position.z + prevHillRadius;
-                        hillsLength += radius;
-                    
-                        positions.push({
-                            z: hill.position.z,
-                            radius: radius
-                        });
-
-                        addHill = true;
-                        break;
-                    case 2:
-                        let predictedDistanceToTrack = hillArray[i - 1].position.x + (prevHillRadius * 2);
-                        if (radius < prevHillRadius) {
-                            if (predictedDistanceToTrack <= -4) {
-                                switch (Math.floor((Math.random() * 1) + 1)) {
-                                    case 1:
-                                        hill.position.x = hillArray[i - 1].position.x - prevHillRadius; //Place away
-                                        break;
-
-                                    case 2:
-                                        hill.position.x = hillArray[i - 1].position.x + prevHillRadius; //Place to
-                                        break;
-                                }
-                                addHill = true;
-                            } else {
-                                hill.position.x = hillArray[i - 1].position.x - prevHillRadius;
-                                addHill = true;
-                            }
-                        } else {
-                            i--;
-                            addHill = false;
-                        }
-                        break;
-                }
-            } else {
-                i--;
-                addHill = false;
-            }
-        }
-
-        if (addHill == true) {
-            hills.add(hill);
-            hillArray.push(hill);
-            addHill = false;
-        }
-
-    }
-    let hillsObject = {
-        model: hills,
-        length: Math.floor(hillsLength),
-        positions: positions
-    }
-    return hillsObject;
-}*/
-
-function treeGen() {
+function createTree() {
     let tree = new THREE.Group();
 
     let treeHeight = Math.random() * (6 - 3) + 3;
@@ -177,6 +74,9 @@ function treeGen() {
         position += treeHeight * 0.4;
         treeRadius = treeRadius * 0.80;
     }
-
-    return tree;
+    obj = {
+        model: tree,
+        radius: treeRadius
+    }
+    return obj;
 }
